6b207f73cda523129558ddb9a8b66296bfd63e5a
glued <rwworsfold@gmail.com>
2016-01-16 16:13:44 -0800

    add video aspect ratio resize method

--- src/video/index.js
+++ src/video/index.js
@@ -4,3 +4,25 @@
 //     //TODO: FINISH
 //   }
 // }
+
+export function resizeCover(videoWidth, videoHeight, containerWidth, containerHeight){
+  const containerAspectRatio  = containerWidth /containerHeight;
+  const videoAspectRatio      = videoWidth / videoHeight;
+
+  let newWidth  = 0;
+  let newHeight = 0;
+  let offsetX   = 0;
+  let offsetY   = 0;
+
+  if(containerAspectRatio > videoAspectRatio){
+    newWidth  = containerWidth;
+    newHeight = ~~(newWidth / videoAspectRatio);
+    offsetY   = -(newHeight >> 1) + (containerHeight >> 1);
+  }else{
+    newHeight = containerHeight;
+    newWidth  = ~~(newHeight * videoAspectRatio);
+    offsetX   = -(newWidth >> 1) + (containerWidth >> 1);
+  }
+
+  return [newWidth, newHeight, offsetX, offsetY];
+}
