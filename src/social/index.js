4e7f5ce5ca9b82f90dcafdfa7249f38e7fb67dbb
glued <rwworsfold@gmail.com>
2016-01-18 13:03:14 -0800

    social tweaks

--- src/social/index.js
+++ src/social/index.js
@@ -2,17 +2,19 @@ function openSocialShareDialog(url, suffix = '', href = document.location.href){
   window.open(`${url}${href}${suffix}`, 'targetWindow', 'width=600, height=400');
 }
 
-export function attachFacebookShareDialog(element, href){
+export function attachFbShareDialog(element){
+  if(!element) return;
   element.addEventListener('click', e => {
     e.preventDefault();
-    openSocialShareDialog('https://www.facebook.com/sharer/sharer.php?u=', '', href);
+    openSocialShareDialog('https://www.facebook.com/sharer/sharer.php?u=');
   }, false);
 }
 
-export function attachTwitterShareDialog(element, via, href){
-  if(via) via = `&via=${via}`;
+export function attachTwShareDialog(element, via){
+  if(!element) return;
+  via = `&via=${via}`;
   element.addEventListener('click', e => {
     e.preventDefault();
-    openSocialShareDialog('https://twitter.com/intent/tweet?url=', via, href);
+    openSocialShareDialog('https://twitter.com/intent/tweet?url=', via);
   }, false);
 }
