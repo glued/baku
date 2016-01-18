d73dd06b150b0955694ef71a47ffebacf26276d1
glued <rwworsfold@gmail.com>
2016-01-17 16:13:20 -0800

    override href social

--- src/social/index.js
+++ src/social/index.js
@@ -2,17 +2,17 @@ function openSocialShareDialog(url, suffix = '', href = document.location.href){
   window.open(`${url}${href}${suffix}`, 'targetWindow', 'width=600, height=400');
 }
 
-export function attachFacebookShareDialog(element){
+export function attachFacebookShareDialog(element, href){
   element.addEventListener('click', e => {
     e.preventDefault();
-    openSocialShareDialog('https://www.facebook.com/sharer/sharer.php?u=');
+    openSocialShareDialog('https://www.facebook.com/sharer/sharer.php?u=', '', href);
   }, false);
 }
 
-export function attachTwitterShareDialog(element, via){
+export function attachTwitterShareDialog(element, via, href){
   if(via) via = `&via=${via}`;
   element.addEventListener('click', e => {
     e.preventDefault();
-    openSocialShareDialog('https://twitter.com/intent/tweet?url=', via);
+    openSocialShareDialog('https://twitter.com/intent/tweet?url=', via, href);
   }, false);
 }
