4afb9777259f29962a7df51fc013e8dbbf4eb555
glued <rwworsfold@gmail.com>
2016-04-20 18:20:50 -0700

    social adjustments

--- src/social/index.js
+++ src/social/index.js
@@ -1,20 +1,22 @@
-function openSocialShareDialog(url, suffix = '', href = document.location.href){
-  window.open(`${url}${href}${suffix}`, 'targetWindow', 'width=600, height=400')
+function openSocialShareDialog(url, path = '', suffix = '', href = document.location.href){
+  window.open(`${url}${href}${path}${suffix}`, 'targetWindow', 'width=600, height=400')
 }
 
 export function attachFbShareDialog(element){
   if(!element) return
   element.addEventListener('click', e => {
     e.preventDefault()
-    openSocialShareDialog('https://www.facebook.com/sharer/sharer.php?u=')
+    let path = e.currentTarget.getAttribute('href')
+    openSocialShareDialog('https://www.facebook.com/sharer/sharer.php?u=', path)
   }, false)
 }
 
-export function attachTwShareDialog(element, via){
+export function attachTwShareDialog(element, author){
   if(!element) return
-  via = `&via=${via}`
+  let suffix = `&via=${author}`
   element.addEventListener('click', e => {
     e.preventDefault()
-    openSocialShareDialog('https://twitter.com/intent/tweet?url=', via)
+    let path = e.currentTarget.getAttribute('href')
+    openSocialShareDialog('https://twitter.com/intent/tweet?url=', path, suffix)
   }, false)
 }
