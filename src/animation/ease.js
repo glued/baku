53ebc08c8f20293cb57c708c422e2121e4b54330
glued <rwworsfold@gmail.com>
2016-04-30 10:56:33 -0700

    revised easing

--- src/animation/ease.js
+++ src/animation/ease.js
@@ -1,46 +1,41 @@
 // easing functions from "Tween.js" / Robert Penner modified for ES6
 //createjs.com/tweenjs
+export default{
+  linear:n => n,
 
-export function linear(n){
-  return n
-}
+  inBack:n => {
+    const s = 1.70158
+    return n * n * ((s + 1) * n - s)
+  },
 
-export function inBack(n){
-  const s = 1.70158
-  return n * n * ((s + 1) * n - s)
-}
+  outBack:n => {
+    const s = 1.70158
+    return --n * n * ((s + 1) * n + s) + 1
+  },
 
-export function outBack(n){
-  const s = 1.70158
-  return --n * n * ((s + 1) * n + s) + 1
-}
+  inOutBack:n => {
+    const s = 2.5949095
+    const s1 = s + 1
+    n *= 2
+    if(n < 1) return 0.5 * (n * n * (s1 * n - s))
+    n -= 2
+    return 0.5 * (n * n * (s1 * n + s) + 2)
+  },
 
-export function inOutBack(n){
-  const s = 1.70158 * 1.525
-  const s1 = s + 1
-  n *= 2
-  if(n < 1) return 0.5 * (n * n * (s1 * n - s))
-  n -= 2
-  return 0.5 * (n * n * (s1 * n + s) + 2)
-}
+  inCube: n => n * n * n,
 
-export function inCube(n){
-  return n * n * n
-}
+  outCube: n => --n * n * n + 1,
 
-export function outCube(n){
-  return --n * n * n + 1
-}
+  inOutCube: n => {
+    n *= 2
+    if (n < 1) return 0.5 * n * n * n
+    return 0.5 * ((n -= 2) * n * n + 2)
+  },
 
-export function inOutCube(n){
-  n *= 2
-  if (n < 1) return 0.5 * n * n * n
-  return 0.5 * ((n -= 2) * n * n + 2)
-}
-
-export function inOutExpo(n){
-  if(n === 0) return 0
-  if(n === 1) return 1
-  if((n *= 2) < 1) return 0.5 * Math.pow(1024, n - 1)
-  return 0.5 * (-Math.pow(2, -10 * (n - 1)) + 2)
+  inOutExpo: n => {
+    if(n === 0) return 0
+    if(n === 1) return 1
+    if((n *= 2) < 1) return 0.5 * Math.pow(1024, n - 1)
+    return 0.5 * (-Math.pow(2, -10 * (n - 1)) + 2)
+  }
 }
